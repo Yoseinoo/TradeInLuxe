e2afe0cc1205ea9871562dc55ac5caf5b14db19e
Martin <martinsimongo@gmail.com>
2024-03-02 20:14:42 +0100

    Mise en place de webpack-encore et de stimulus pour le js

---
+++ www/assets/controllers/test_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  connect() {
+    console.log("HELLO BG, on va pouvoir faire du js propre 😶‍🌫️");
+  }
+}
